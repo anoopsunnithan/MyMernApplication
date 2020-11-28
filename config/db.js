40692f67938ee68f0bef9126e58de67e075722be
anoopsunnithan <anoopsunnithan@gmail.com>
2020-11-28 23:58:35 +0530

    POST operation Check
    Post operation on  user registration.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
-
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
-
+        await mongoose.connect(db, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
         console.log('MongoDB Connected...');
     } catch(error) {
         console.log(error.message);
--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define routes 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
 
 const PORT = process.env.PORT || 5000;
 
