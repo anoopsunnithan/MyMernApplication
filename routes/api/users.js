0c957f309d8b4409a26ba49b620a59d3071797a4
anoopsunnithan <anoopsunnithan@gmail.com>
2020-11-29 01:37:25 +0530

    Auth middleware & JWT verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, vaidationResult, validationResult } = require('express-validator');
+
+//importing user model
+const User = require('../../models/User');
 
 //@route POST api/users
 //@desc Register user
@@ -14,12 +21,67 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters')
     .isLength({ min:6 })
 ], 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array()});
+        return res
+        .status(400)
+        .json({ errors: errors.array()});
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+    // check user exists
+
+    let user = await User.findOne({ email });
+
+    if(user) {
+        res.status(400).json({ errors: [{msg:'User already exists'}] });
+    }
+
+    //Get users gravitar
+
+    const avatar = gravatar.url( email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    } );
+
+    user = new User({
+        name,
+        email,
+        avatar,
+        password
+    });
+    //Encrypt password
+
+    const salt = await bcrypt.genSalt(10);    
+    user.password = await bcrypt.hash(password, salt); 
+
+    await user.save();
+
+    //Return jsonwebtoken
+
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (error, token) => {
+            if (error) throw error;
+            res.json({ token });
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 
