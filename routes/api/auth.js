73e1e7ab43abb9f50337ea1e947b36de0665df38
anoopsunnithan <anoopsunnithan@gmail.com>
2020-11-29 23:24:51 +0530

    Get Current User profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, vaidationResult, validationResult } = require('express-validator');
 
 //@route GET api/auth
 //@desc Test route
@@ -10,6 +14,63 @@ router.get('/', auth, async(req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
         res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//@route POST api/auth
+//@desc Authenticate user and get token
+//@access Public
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required.').exists()
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+        .status(400).json({ errors: errors.array()});
+    }
+
+    const { email, password } = req.body;
+
+    try {
+    // Checking for user
+    let user = await User.findOne({ email });
+    if(!user) {
+        return res
+        .status(400)
+        .json({ errors: [{msg:'Invalid Credentials'}] });
+    }
+   
+    //Compare Passwords
+    const isMatch = await bcrypt.compare(password,user.password);
+    
+    if(!isMatch) {
+        return res
+        .status(400)
+        .json({ errors: [{msg:'Invalid Credentials'}] });
+    }
+
+    //Return jsonwebtoken
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (error, token) => {
+            if (error) throw error;
+            res.json({ token });
+        });
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server Error');
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,80 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@route GET api/Profile/me
+//@desc Get current user profile
+//@access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Ther is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 //@route GET api/Profile
-//@desc Test route
-//@access Public
-router.get('/', (req, res) => res.send('Profile route'));
+//@desc create/update profile
+//@access Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skilss is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      instagram,
+      twitter,
+      likedin,
+    } = req.body;
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+
+    console.log(profileFields.skills);
+    res.send('Hello');
+  }
+);
 
 module.exports = router;
