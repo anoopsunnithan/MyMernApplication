a74a3c97a6bc99a36dd6b5427930dce91a4aa630
anoopsunnithan <anoopsunnithan@gmail.com>
2020-12-08 00:03:05 +0530

    Code For testing

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
-import spinner from './spinner.gif';
-
-export default () => {
-  <Fragment>
+import React from "react";
+import spinner from "./spinner.gif";
+ 
+export default function Spinner() {
+  return (
     <img
       src={spinner}
-      style={{ width: '200px', margin: 'auto', display: 'block' }}
-      alt={'Loading...'}
+      style={‌{ width: "200px", margin: "auto", display: "block" }}
+      alt={"Loading..."}
     />
-  </Fragment>;
-};
+  );
+}
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -14,13 +14,11 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
-      return [
-        {
-          ...state,
-          payload: payload,
-          loading: false,
-        },
-      ];
+      return {
+        ...state,
+        payload: payload,
+        loading: false,
+      };
     case PROFILE_ERROR:
       return {
         ...state,
