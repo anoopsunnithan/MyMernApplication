fc05681e778357ac418347b55d02d01477030861
Anoop S Unnithan <anoopsunnithan@gmail.com>
2020-12-24 22:08:08 +0530

    Profile Error Corrected

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,7 +131,7 @@ router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.params.user_id,
-    }).populate('User', []);
+    }).populate('user', ['name', 'avatar'], 'User');
 
     if (!profile) {
       return res.status(400).json({ msg: 'Profile not found' });
