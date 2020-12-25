9cc393dab00b1c6b9d1a1de4c57e3ebd134cf497
Anoop S Unnithan <anoopsunnithan@gmail.com>
2020-12-25 23:41:11 +0530

    Started developing posts and related components

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -25,3 +25,6 @@ export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 
 export const GET_REPOS = 'GET_REPOS';
+
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,22 @@
+import React, { useEffect, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { getPosts } from '../../actions/post';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return <div>ba ba</div>;
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
