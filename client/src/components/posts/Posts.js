3e440d5be91e09566fe22e5a7a6a1f47a48bd739
Anoop S Unnithan <anoopsunnithan@gmail.com>
2020-12-28 23:13:38 +0530

    Posttem Component Development

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,13 +2,29 @@ import React, { useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import PropTypes from 'prop-types';
 
-const Posts = ({ getPosts, posts: { post, loading } }) => {
+const Posts = ({ getPosts, posts: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div>ba</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/* PostFrom */}
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={posts._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
@@ -16,7 +32,7 @@ Posts.propTypes = {
   posts: PropTypes.object.isRequired,
 };
 const mapStateToProps = (state) => ({
-  post: state.posts,
+  posts: state.posts,
 });
 
 export default connect(mapStateToProps, { getPosts })(Posts);
