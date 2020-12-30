a74bcbcdd2961f245bfe83b3a7c1072fc53f2dd7
Anoop S Unnithan <anoopsunnithan@gmail.com>
2020-12-30 23:56:01 +0530

    Displaying comment and deleting comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
-
 import { getPost } from '../../actions/post';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, posts: { post, loading }, match }) => {
   useEffect(() => {
@@ -21,6 +21,11 @@ const Post = ({ getPost, posts: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
